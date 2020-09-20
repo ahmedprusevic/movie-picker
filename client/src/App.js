2cd65e557463b19da870384533ee7add48fc9aa6
Ahmed Prusevic <ahmedprusevic@gmail.com>
2020-09-20 22:17:57 +0200

    starting to work on frontend

---
+++ client/src/App.js
@@ -0,0 +1,12 @@
+import React, { Fragment } from 'react';
+import './App.css';
+import Navbar from './components/Navbar'
+
+const App = () => 
+    <Fragment>
+      <Navbar />
+      <h1>App</h1>
+    </Fragment>
+  
+
+export default App;
